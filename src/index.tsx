9a7d77464a6f0ad72f888c897598507e1cd53198
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-02-07 18:00:17 +0200

    Remove template files

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
