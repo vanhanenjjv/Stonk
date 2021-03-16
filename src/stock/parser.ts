d5c9b9a461dea9add39f5bc1d07a3b057f04b5f7
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-03-16 03:32:57 +0200

    Prevent invalid input

--- src/stock/parser.ts
+++ src/stock/parser.ts
@@ -3,17 +3,26 @@ import { StockData } from '../types';
 
 
 export function readCsv(csv: string): StockData[] {
-  return csv
-    .split('\n')
-    .slice(1)
-    .filter(line => line)
-    .map(readStockData)
-    .sort(compareByDate)
-    .map(addSimpleMovingAverage5);
+  try {
+    return csv
+      .split('\n')
+      .slice(1)
+      .filter(line => line)
+      .map(readStockData)
+      .sort(compareByDate)
+      .map(addSimpleMovingAverage5);
+  } catch (error) {
+    console.error(`Unable to parse line: '${error}'.`);
+
+    return [];
+  }
 }
 
 function readStockData(line: string): StockData {
   const [date, close, volume, open, high, low] = line.split(',').map(field => field.trim());
+
+  if (!(date && close && volume && open && high && low))
+    throw new Error(line);
 
   return {
     date: dayjs(date),
